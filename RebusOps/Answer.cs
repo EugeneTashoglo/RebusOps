0724516d53c58edee22afea677d8e917279ab86c
tasho <tasho@DESKTOP-OMM4AD8>
2023-03-12 15:37:05 +0200

    Добавьте файлы проекта.

---
+++ RebusOps/Answer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RebusOps
+{
+    public class Answer
+    {
+        public string Text { get; set; }
+        public byte[] ImageData { get; set; }
+
+        public Answer(string text, byte[] imageData)
+        {
+            Text = text;
+            ImageData = imageData;
+        }
+    }
+
+}
